7707ecea1e865620451b98eb5e4632bd66c6dfd2
emestiza <emestiza@u.rochester.edu>
2021-02-21 16:02:41 -0500

    project info page

---
+++ src/components/ProjectInfo/ProjectInfo.js
@@ -0,0 +1,93 @@
+import React, { Fragment } from 'react'
+
+const ProjectInfo = () => (
+  <Fragment>
+
+    {/* Image */}
+
+    <br>
+    </br>
+    
+    <button type="button" class="btn btn-secondary">Back</button>
+
+    <br>
+    </br>
+
+    <h2 className="text-left">Plant-a-thon</h2>
+
+    <br>
+    </br>
+
+    <div className="container">
+      <p>
+        Adopting a tree in your backyard is a unique way to be involved in your community while helping to increase the city's urban forest canopy.
+        St. Mary's Park is constantly working to make the park beautiful and lush green  for our community we dearly care about. 
+        This summer we are dedicated to grow 1000 more trees and with the help of generaous neighbors and donors like you, we almost meet our target.   
+        Become a part of a family by adopting aor donating a tree and bring home healthier air quality. 
+      </p>      
+    </div>
+
+    <br>
+    </br>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          One of two columns
+        </div>
+        <div className="col-sm">
+          One of two columns
+        </div>
+      </div>
+    </div>
+
+    <br>
+    </br>
+
+    <div className="container">
+      <button type="button" class="btn btn-success">Select</button>
+    </div>
+
+    <br>
+    </br>
+
+    <h3 className="text-left">Other Ways to get involved</h3>
+
+    <br>
+    </br>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          One of three columns
+        </div>
+        <div className="col-sm">
+          One of three columns
+        </div>
+        <div className="col-sm">
+          One of three columns
+        </div>
+      </div>
+    </div>
+
+    <br>
+    </br>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          One of three columns
+        </div>
+        <div className="col-sm">
+          One of three columns
+        </div>
+        <div className="col-sm">
+          One of three columns
+        </div>
+      </div>
+    </div>
+
+  </Fragment>
+)
+
+export default ProjectInfo
